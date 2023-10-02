bdaad4a4fdd112afe63e3cc05cf9c6538f081a3d
JuniorWSP <juniorwender@yahoo.com.br>
2023-10-02 18:29:33 -0300

    Aula11

---
+++ aula11.py
@@ -0,0 +1,7 @@
+print('\033[4;29;42m Olá, Mundo!\033[m')
+print('\033[7;30m Olá, Mundo!\033[m')
+a = 3
+b = 5
+print('Os valores são \033[32m {} \033[m e \033[31m {} \033[m !!!'.format(a, b))
+nome = 'Junior'
+print('Seu nome é {} {} {}!!!'.format('\033[3;30;47m', nome, '\033[m'))
