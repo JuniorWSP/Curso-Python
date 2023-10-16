aa2533e1e650598d80e916e3df97f350664dd5f1
JuniorWSP <juniorwender@yahoo.com.br>
2023-10-16 20:38:50 -0300

    Concluindo exercicios Aula13

--- exercicio53-aula13.py
+++ exercicio53-aula13.py
@@ -1,2 +1,13 @@
 #Crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando
 #os espaços. Ex: pode ser lido de traz pra frente.
+frase = str(input('Digite uma frase: ')).strip().upper()   #strip retira os espaços, upper coloca maiuscula
+palavras = frase.split()    #divide as palavras
+junto = ''.join(palavras)   #junta as palavras
+inverso = ''
+for letra in range (len(junto) -1, -1, -1): #fez-se o inverso indo da ultima letra ate a primeira voltando uma letra
+    inverso = inverso + junto[letra]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
--- exercicio54-aula13.py
+++ exercicio54-aula13.py
@@ -1,5 +1,15 @@
 #Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas
 #pessoas ainda não atingiram a maioridade e quantas já são maiores.
 from datetime import date
-ano = int(input('Digite seu ano de nascimento: '))
-
+atual = date.today().year
+totalmaior = 0
+totalmenor = 0
+for pessoa in range(1, 8):
+    nasc = int(input('Em que ano a {}° pessoa nasceu? '.format(pessoa)))
+    idade = atual - nasc
+    if idade >= 21:
+        totalmaior = totalmaior + 1
+    else:
+        totalmenor = totalmenor + 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totalmaior))
+print('E também tivemos {} pesosas menores de idade.'.format(totalmenor))
