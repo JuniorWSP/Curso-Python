de272a1b3d0ce2bc9fd0b73022c5d763fb1bb208
JuniorWSP <juniorwender@yahoo.com.br>
2023-10-18 20:09:43 -0300

    Exercicios aula14

---
+++ exercicio58-aula14.py
@@ -0,0 +1,3 @@
+#Melhore o jogo do Desafio 28 onde o computador vai 'pensar' em um número entre 0 a 10.
+#Só que agora o jogador vai tentar advinhar até acertar, mostrando no final quantos palpites foram
+#necessários para vencer.
