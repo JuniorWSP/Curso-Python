ada8021914e9c21d86358e5b7a22815e57442386
JuniorWSP <juniorwender@yahoo.com.br>
2023-10-21 18:49:29 -0300

    exercicio aula14

--- exercicio60-aula14.py
+++ exercicio60-aula14.py
@@ -1,1 +1,8 @@
-#Faça um programa que leia um número qualquer e mosre seu fatorial.
+#Faça um programa que leia um número qualquer e mostre seu fatorial.
+fat = int(input('Digite um número: '))
+f = 1
+fatorial = fat
+while fat > 1:
+    f = f * fat
+    fat = fat - 1
+print('O fatorial de {} é {}'.format(fatorial, f))
--- exercicio61-aula14.py
+++ exercicio61-aula14.py
@@ -1,2 +1,9 @@
 #Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
 #da progressão usando a estrutura while.
+termo = int(input('Digite o 1° termo de uma PA: '))
+razao = int(input('Digite a razão da PA: '))
+t = 0
+while t < 10:
+    print('{}° termo é {}'.format(t + 1, termo))
+    termo = termo + razao
+    t = t + 1
