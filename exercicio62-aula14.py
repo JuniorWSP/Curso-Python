de9cc20af9798acf92c5ea691ef068ec60b9b6c7
JuniorWSP <juniorwender@yahoo.com.br>
2023-10-24 20:53:32 -0300

    finalizando exercicios aula14

--- exercicio62-aula14.py
+++ exercicio62-aula14.py
@@ -2,9 +2,15 @@
 #O programa encerra quando ele disser que quer mostrar 0 termos.
 termo = int(input('Digite o 1° termo de uma PA: '))
 razao = int(input('Digite a razão da PA: '))
-t = 0
-while t < 10:
-    print('{}° termo é {}'.format(t + 1, termo))
-    termo = termo + razao
-    t = t + 1
-
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{}° termo é {}'.format(cont + 1, termo))
+        termo = termo + razao
+        cont = cont + 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
--- exercicio63-aula14.py
+++ exercicio63-aula14.py
@@ -1,3 +1,17 @@
 #Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros
 #elementos de uma Sequência de Fibonacci. EX: 0 - 1 - 1 - 2 - 3 - 5 - 8
-n = int(input('Digite um número inteiro: '))
+print('-=' * 30)
+print('Sequência de Fibonacci')
+print('-=' * 30)
+n = int(input('Quantos termos você quer mostrar? '))
+n1 = 0
+n2 = 1
+print('{} ➜ {}'.format(n1, n2), end='')
+cont = 3
+while cont <= n:
+    n3 = n1 + n2
+    print(' ➜ {}'.format(n3), end='')
+    n1 = n2
+    n2 = n3
+    cont = cont + 1
+print(' ➜ FIM')
