b3e939a7b17add81ab72e5d4405939e3f2ac12c5
JuniorWSP <juniorwender@yahoo.com.br>
2023-11-01 21:01:38 -0300

    exercicios aula15

--- exercicio66-aula15.py
+++ exercicio66-aula15.py
@@ -1,7 +1,18 @@
 #Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando
 #o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram
 #digitados e qual foi a soma entre eles (desconsiderando o flag).
-num = 0
+soma = 0
+cont = 0
+while True:
+    num = int(input('Digite um valor (999 para parar): '))
+    if num == 999:
+        break
+    cont = cont + 1
+    soma = soma + num
+print(f'A soma dos {cont} valores foi {soma}!')
+
+
+'''num = 0
 cont = 0
 soma = 0
 while num != 999:
@@ -10,4 +21,4 @@ while num != 999:
         break
     cont = cont + 1
     soma = soma + num
-print(f'Foram digitados {cont} e sua soma foi {soma}')
+print(f'Foram digitados {cont} e sua soma foi {soma}')'''
--- exercicio67-aula15.py
+++ exercicio67-aula15.py
@@ -1,6 +1,17 @@
 #Faça um programa que mostre a tabuada de vários números um de cada vez, para cada valor
 #digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    print('-' * 40)
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n} X {c} = {n * c}')
+    print('-' * 40)
+print('PROGRAMA TABUADA ENCERRADO... Volte sempre!')
+
+
+'''while True:
     num = int(input('Quer ver a tabuada de qual valor? '))
     if num < 0:
         print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
@@ -11,4 +22,4 @@ while True:
         mult = num * tab
         print(f'{num} X {tab} = {mult}')
         tab = tab + 1
-    print('=' * 40)
+    print('=' * 40)'''
